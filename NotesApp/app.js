60aa6e8b106b986054cc420f7cfd639935661042
Breno Rezende <brenorrezende@gmail.com>
2017-12-07 11:02:04 -0300

    Notes CRUD added

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -8,17 +8,29 @@ const notes = require('./notes');
 let argv = yargs.argv;
 let command = argv._[0];
 
+let logNote = (note) => {
+    if (note) {
+        console.log('====================================');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+        console.log('====================================');
+    } else {
+        console.log("Note not found")
+    }
+};
+
 switch (command) {
     case 'add':
         let added = notes.addNote(argv.title, argv.body);        
-        added? console.log('Note created successfully'): console.log('Note title already exists');
+        added ? console.log('Note created successfully') : console.log('Note title already exists');
         break;
     case 'list':
         let allNotes = notes.getAll();
-        console.log(allNotes);
+        allNotes.forEach(note => logNote(note));
         break;
     case 'read':
-        notes.getNote(argv.title);
+        let note = notes.getNote(argv.title);
+        logNote(note);
         break;
     case 'remove':
         let removed = notes.removeNote(argv.title);
